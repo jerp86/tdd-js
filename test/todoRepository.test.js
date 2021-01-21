cac11de1132bd0165828f270b0959bf8d77276a7
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-21 06:41:00 -0300

    ensure "find" called once

--- test/todoRepository.test.js
+++ test/todoRepository.test.js
@@ -1,27 +1,52 @@
-const { describe, it, before } = require('mocha');
+const { describe, it, before, afterEach } = require('mocha');
 const { expect } = require('chai');
+const { createSandbox } = require('sinon');
 
 const TodoRepository = require('../src/todoRepository');
 
-const mockDatabase = [
-  {
-    name: 'Xuxa da Silva',
-    age: 90,
-    meta: { revision: 0, created: 1611220616093, version: 0 },
-    '$loki': 1
-  },
-  {
-    name: 'Joaozinho',
-    age: 50,
-    meta: { revision: 0, created: 1611220616094, version: 0 },
-    '$loki': 2
-  }
-];
-
 describe('todoRepository', () => {
-  describe('methods signature', () => {
-    it('should call insertOne from lokijs');
+  let todoRepository;
+  let sandbox;
 
-    it('should call find from lokijs');
+  before(() => {
+    todoRepository = new TodoRepository();
+    sandbox = createSandbox()
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('methods signature', () => {
+    it('should call find from lokijs', () => {
+      const mockDatabase = [
+        {
+          name: 'Xuxa da Silva',
+          age: 90,
+          meta: { revision: 0, created: 1611220616093, version: 0 },
+          '$loki': 1
+        },
+        {
+          name: 'Joaozinho',
+          age: 50,
+          meta: { revision: 0, created: 1611220616094, version: 0 },
+          '$loki': 2
+        }
+      ];
+      
+      const functionName = 'find';
+      const expectedReturn = mockDatabase;
+
+      sandbox.stub(
+        todoRepository.schedule,
+        functionName,
+      ).returns(expectedReturn);
+
+      const result = todoRepository.list();
+      expect(result).to.be.deep.equal(expectedReturn);
+      expect(todoRepository.schedule[functionName].calledOnce).to.be.ok;
+    });
+    
+    it('should call insertOne from lokijs');
   })
 });
