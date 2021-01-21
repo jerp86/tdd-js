83adc92b42ebd0600aba969dffb3ee5a8ffabf7c
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-21 05:48:24 -0300

    add todo tests

---
+++ test/todo.test.js
@@ -0,0 +1,13 @@
+const { describe, it, before } = require('mocha');
+const { expect } = require('chai');
+const Todo = require('../src/todo');
+
+describe('todo', () => {
+  describe('#isValid', () => {
+    it('should return invalid when creating an object without text', () => {});
+
+    it('should return invalid when creating an object using "when" property invalidy', () => {});
+
+    it('should have "id", "text", "when" and "status" properties after creating object', () => {});
+  });
+});
