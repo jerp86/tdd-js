3ae1523613247fd0226348c757b8eb989a622651
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-22 21:31:00 -0300

    shouldn\'t save todo item with invalid data

--- src/todoService.js
+++ src/todoService.js
@@ -2,12 +2,22 @@ class TodoService {
   constructor({ todoRepository }) {
     this.todoRepository = todoRepository;
   }
-
-  list(query) {
-    return this.todoRepository.list().map(({ meta, $loki, ...result }) => result);
-  }
   
-  create(todoItem) {}
+    list(query) {
+      return this.todoRepository.list().map(({ meta, $loki, ...result }) => result);
+    }
+  
+  create(todoItem) {
+    console.log('item-', isNaN(todoItem.when))
+    if (!todoItem.isValid()) {
+      return {
+        error: {
+          message: 'invalid data',
+          data: todoItem,
+        },
+      };
+    }
+  }
 }
 
 module.exports = TodoService;
--- test/todoService.test.js
+++ test/todoService.test.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 const { createSandbox } = require('sinon');
 
 const TodoService = require('../src/todoService');
+const Todo = require('../src/todo');
 
 describe('todoService', () => {
   let sandbox;
@@ -52,7 +53,26 @@ describe('todoService', () => {
       todoService = new TodoService(dependencies);
     });
 
-    it('shouldn\'t save todo item with invalid data');
+    it('shouldn\'t save todo item with invalid data', () => {
+      const data = new Todo({
+        text: '',
+        when: ''
+      });
+
+      // O Reflect é mais performático do que o "delete data.id"
+      Reflect.deleteProperty(data, 'id');
+
+      const expected = {
+        error: {
+          message: 'invalid data',
+          data: data,
+        },
+      };
+
+      const result = todoService.create(data);
+
+      expect(result).to.be.deep.equal(expected);
+    });
     
     it('should save todo item with late status when the property is further than today');
     
