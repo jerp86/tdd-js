1b5b2ed3c3d91d86dc6f67d566f4ea5ea50038b0
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-22 21:58:54 -0300

    ensure create an object with all properties

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,9 +1,17 @@
-const { describe, it, before } = require('mocha');
+const { describe, it, beforeEach, afterEach } = require('mocha');
 const { expect } = require('chai');
+const { createSandbox } = require('sinon');
 
 const Todo = require('../src/todo');
 
 describe('todo', () => {
+  let sandbox;
+  beforeEach(() => {
+    sandbox = createSandbox();
+  });
+
+  afterEach(() => sandbox.restore());
+
   describe('#isValid', () => {
     it('should return invalid when creating an object without text', () => {
       const data = {
@@ -34,11 +42,24 @@ describe('todo', () => {
         text: 'Hello World',
         when: new Date("2021-01-21"),
       };
+      const expectedId = '000001';
+      
+      const uuid = require('uuid');
+      const fakeUUID = sandbox.fake.returns(expectedId);
+      sandbox.replace(uuid, uuid.v4.name, fakeUUID);
 
       const todo = new Todo(data);
+      const expectedItem = {
+        text: data.text,
+        when: data.when,
+        status: '',
+        id: expectedId,
+      };
       const result = todo.isValid();
 
       expect(result).to.be.ok;
+      expect(uuid.v4.calledOnce).to.be.ok;
+      expect(todo).to.be.deep.equal(expectedItem);
     });
   });
 });
